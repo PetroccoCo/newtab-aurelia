9b5488af9e3fb9db5c64ca284a41514d3da95fb3
Pete Winterscheidt <pete.winterscheidt@readytalk.com>
2017-01-31 16:37:06 -0700

    Adding moment.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
+import moment from 'moment';
 export class App {
+  updateTime(context) {
+    context.time = moment().format("LTS");
+  }
   constructor() {
     this.message = 'Hello World!';
+    this.updateTime(this);
+  }
+  attached() {
+    this.interval = setInterval(this.updateTime, 1000, this);
   }
 }
